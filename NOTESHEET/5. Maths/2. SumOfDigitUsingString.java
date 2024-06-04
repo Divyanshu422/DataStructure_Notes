346bf9f3b0371f3bc14f3aaf9d6fe8fa881b9ff4
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-05 01:11:17 +0530

    Adding the Digit using the String Method

---
+++ NOTESHEET/5. Maths/2. SumOfDigitUsingString.java
@@ -0,0 +1,28 @@
+// ! Implementing the addition of Digit using String method.
+/*
+            Method used in solving the proble -> charAt() and length()
+*/
+
+class Program1 {
+    public static void DigitSumString(String str) {
+        /*
+         * Since the input to the method is String -> So there is 2 method used in
+         * String:
+         * 1. charAt() => used to access the String character
+         * 2. length() => for size of string
+         */
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i); // output -> '1' '2' '3' '4' '5' which in int format is 49, 50, 51, 52, 53.
+                                     // we dont want to add 49,50,51,52,53. rather we want to add 1,2,3,4,5.
+                                     // so to achieve this we need to subtract the ch with 48 or '0'
+            sum = sum + (ch - '0');
+        }
+        System.out.println(sum); // 15
+
+    }
+
+    public static void main(String[] args) {
+        DigitSumString("12345");
+    }
+}
