13b2c5975622c9d65c0bf8173b5a4b9169ecac4d
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-18 11:16:41 +0530

    Pattern3

---
+++ NOTESHEET/2. PatternNoteBook/Problem3.java
@@ -0,0 +1,24 @@
+/*
+ *          1 
+ *          2 3 
+ *          4 5 6
+ *          7 8 9 10
+ */
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt(); // * Consuming the input from the user
+        int printingChar = 1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < (i + 1); j++) {
+                System.out.print(printingChar + " ");
+                printingChar++;
+            }
+            System.out.println();
+
+        }
+    }
+}
