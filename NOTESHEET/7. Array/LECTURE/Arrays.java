ac2b238753c1d2fae5044ddcb5836c75186f8f35
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-27 18:56:43 +0530

    Declaring and initialising the array

---
+++ NOTESHEET/7. Array/LECTURE/Arrays.java
@@ -0,0 +1,9 @@
+
+
+    class Array1{
+        public static void main(String[] args) {
+            int [] age; //By this line the age variable is stored in the stack but no memory is allocated
+
+            age = new int[5]; // Allocating the memeory => called initialization
+        }
+    }
