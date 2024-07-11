9e7310582ae26392855538ebb2063049c95c3c6c
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-11 12:35:11 +0530

    Printing the arrayList member

--- Collection/Lecture4.java
+++ Collection/Lecture4.java
@@ -8,16 +8,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class CustomArrayList {
+class Program4 {
     Object[] obj = null;
     int index = 0;
 
-    public CustomArrayList(int size) {
+    public Program4(int size) {
         obj = new Object[size];
     }
-    public CustomArrayList() {
+
+    public Program4() {
         obj = new Object[10];
     }
+
     public void add(Object obj) {
         if (index >= this.obj.length) {
             // calculating the newSize
@@ -29,13 +31,34 @@ class CustomArrayList {
         index++;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder printString = new StringBuilder();
+        printString.append("[");
+        for (int i = 0; i < index; i++) {
+            printString.append(this.obj[i]);
+            if (i < this.obj.length - 1 && this.obj[i + 1] != null) {
+                printString.append(", ");
+            }
+        }
+        printString.append("]");
+
+        // When you call toString() on a StringBuilder instance, Java resolves this call
+        // to the toString method defined in the StringBuilder class, which converts the
+        // sequence of characters in the StringBuilder to a String.
+
+        return printString.toString(); // Calls StringBuilder's toString method
+    }
+
     public static void main(String[] args) {
-        CustomArrayList cl = new CustomArrayList(3);
+        Program4 cl = new Program4(3);
         cl.add("Hello");
         cl.add("World");
         cl.add("Java");
         cl.add("Program");
-        cl.add("Program1");     
+        cl.add("Program1");
         cl.add("Program2");
-        System.out.println("Printing the object " + cl); 
+        // System.out.println("Printing the object " + cl);
+        System.out.println("The output is " + cl.toString());
+    }
 }
