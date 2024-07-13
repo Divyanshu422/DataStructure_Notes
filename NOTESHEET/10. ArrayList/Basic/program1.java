ca48e604e8f1f7344bd31cf8cec1e46b2eabef68
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-13 12:32:48 +0530

    Basic of ArrayList

--- NOTESHEET/10. ArrayList/Basic/program1.java
+++ NOTESHEET/10. ArrayList/Basic/program1.java
@@ -1,16 +1,54 @@
 
 /*
- * Adding the element to the list beyound the size initialized. in Arraylist
- * we can add the element but in the array we cant do the same
- */
+    * Adding the element to the list beyound the size initialized. in Arraylist
+    * we can add the element but in the array we cant do the same
+    * In this sheet we are performing the basic method of arrayList:
+                * Defining the syntax of arrayList
+                * add method(E object)
+                * get method(index)
+                * remove method
+                * set method => add particular index
+                * contains method 
+*/
+
+//  Importing the package of arrayList
 import java.util.ArrayList;
 
 class Program1 {
     public static void main(String[] args) {
-        ArrayList list = new ArrayList<>(2);
-        list.add(1);
-        list.add(2);
-        list.add(3); // * Adding the 3rd element but hard coded for 2
-        System.out.println("ArrayList contain following " + list); // ! No error
+
+        // * Syntax of arrayList
+        ArrayList<String> fruits = new ArrayList<>();
+
+        // ! Add method
+        fruits.add("Apple");
+        fruits.add("Orange");
+        fruits.add("Banana");
+        fruits.add("Pineapple");
+        fruits.add("Guava");
+        fruits.add("Peach");
+
+        System.out.println(fruits); // * [Apple, Orange, Banana, Pineapple, Guava, Peach] */
+
+        // ! get method
+        String obj = fruits.get(2); // * Since we have stored the String in the arraYlist so we need to use String
+                                    // not Object variable */
+        System.out.println(obj); // * Banana
+
+        // ! set method
+        fruits.set(5, "pomegranate"); // * [Apple, Orange, Banana, Pineapple, Guava, pomegranate] */
+        System.out.println(fruits);
+
+        // ! Remove method: we can pass index or the value
+        fruits.remove("Orange");
+        System.out.println(fruits); // [Apple, Banana, Pineapple, Guava, pomegranate]
+
+        // ! Contains methods
+        if (fruits.contains("Orange")) {
+            System.out.println("ArrayList contains 'Orange'");
+        } else {
+            System.out.println("ArrayList does not contain 'Orange'");
+        }
+
     }
 }
