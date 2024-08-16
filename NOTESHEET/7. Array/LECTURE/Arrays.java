a85c937ef3cdb4e6bd9b0601c2bd5a379b42a004
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-16 16:55:16 +0530

    Default value in the array

--- NOTESHEET/7. Array/LECTURE/Arrays.java
+++ NOTESHEET/7. Array/LECTURE/Arrays.java
@@ -1,9 +1,25 @@
+/*
+ * Default value stored in the array
+ */
+class Array1 {
 
+    public static void main(String[] args) {
+        int[] array1 = new int[5]; // * 0
+        float[] array2 = new float[5]; // * 0.0
+        boolean[] array3 = new boolean[5]; // * false
 
-    class Array1{
-        public static void main(String[] args) {
-            int [] age; //By this line the age variable is stored in the stack but no memory is allocated
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
-            age = new int[5]; // Allocating the memeory => called initialization
-        }
+        // * Printing the default value */
+        System.out.println("Default value of int array: " + array1[0]);
+        System.out.println("Default value of int array: " + array2[0]);
+        System.out.println("Default value of int array: " + array3[0]);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
+
+}
