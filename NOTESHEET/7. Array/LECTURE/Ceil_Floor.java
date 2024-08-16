f6045fdd4390f97d4a9af0623d04019c94fada27
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-16 17:58:31 +0530

    ceil and floor

---
+++ NOTESHEET/7. Array/LECTURE/Ceil_Floor.java
@@ -0,0 +1,12 @@
+public class Ceil_Floor {
+
+    public static void main(String[] args) {
+
+        float a = 10.99f;
+
+        System.out.println(Math.ceil(a)); // * 11.0
+        System.out.println(Math.floor(a)); // * 10.0
+        System.out.println(Math.round(a)); // * 11
+    }
+
+}
