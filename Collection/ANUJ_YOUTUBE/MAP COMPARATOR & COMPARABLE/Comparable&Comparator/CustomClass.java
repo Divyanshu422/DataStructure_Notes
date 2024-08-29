a936112589d192ad1bf95cbdc23816f2030eaafd
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-29 18:09:03 +0530

    custom class

---
+++ Collection/ANUJ_YOUTUBE/MAP COMPARATOR & COMPARABLE/Comparable&Comparator/CustomClass.java
@@ -0,0 +1,14 @@
+class Animal {
+    // Field variables
+    private String name;
+    private int weight;
+    private int age;
+
+    // Constructor
+    public Animal(String name, int weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+
+}
