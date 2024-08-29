41a82e5372339a06a95898a2348d888e3b6babad
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-29 17:40:23 +0530

    keySet and values

---
+++ Collection/ANUJ_YOUTUBE/MAP COMPARATOR & COMPARABLE/Program2.java
@@ -0,0 +1,31 @@
+
+/*
+ *  using keySet, values and enterie
+ */
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.Set;
+
+class Program2 {
+    public static void main(String[] args) {
+        Map<String, Integer> mapObj = new HashMap<>();
+        mapObj.put("Orange", 100);
+        mapObj.put("Apple", 400);
+        mapObj.put("Banana", 300);
+        mapObj.put("Guava", 200);
+
+        // Extracting all keys -> as Keys are of String type
+        Set<String> keyObj = mapObj.keySet();
+        System.out.println(keyObj); // * [Guava, Apple, Orange, Banana]
+
+        // Extracting all keys -> as Keys are of String type
+        // * Set<Integer> valueObj = mapObj.values(); => wrong becoz The values() method
+        // * returns a Collection<Integer>, not a Set<Integer>.
+        Collection<Integer> valueObj = mapObj.values();
+        System.out.println(valueObj);
+
+    }
+}
