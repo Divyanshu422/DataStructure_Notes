37b24d49b07dd4f0a0f1c590375ce783d3d64d16
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-29 18:12:45 +0530

    Designing class to learn the comparator and comparable -=> instantiating the Custom class

---
+++ Collection/ANUJ_YOUTUBE/MAP COMPARATOR & COMPARABLE/Comparable&Comparator/LearComparator&Comparable.java
@@ -0,0 +1,9 @@
+
+class Program {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("Lion", 230, 5);
+        Animal animal2 = new Animal("Tiger", 220, 15);
+        Animal animal3 = new Animal("Elephant", 5000, 25);
+        Animal animal4 = new Animal("Mouse", 14, 3);
+    }
+}
