7c9edd7d322288ec3e513e61e1c80e1460fbb451
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-03 17:04:09 +0530

    LinkedList'

---
+++ Collection/LinkedList/Program1.java
@@ -0,0 +1,58 @@
+/*
+ * Writing the code for the LinkedList class
+ */
+
+class CustomList {
+
+    class CustomNode {
+        Object data;
+        CustomNode next;
+
+        // Constructor
+        public CustomNode(Object data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    CustomNode head; // * Default value = null
+    int size; // * Default value = 0
+
+    public void addNode(Object data) {
+        // * Creating the Node when addNode is called
+        CustomNode nodeObj = new CustomNode(data);
+
+        // * Checking whether head points to null or any other value
+        if (head == null) {
+            head = nodeObj;
+        }
+        // * If head is not null, then we need to traverse the list to find the last
+        else {
+            CustomNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = nodeObj;
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (head == null)
+            return "[ ]";
+        else {
+            CustomNode temp = head;
+            sb.append("[ ");
+            while (temp.next != null) {
+                sb.append(temp.data + ", ");
+                temp = temp.next;
+            }
+            sb.append(temp.data);
+            sb.append(" ]");
+        }
+        return sb.toString();
+    }
+
+}
---
+++ Collection/LinkedList/Program2.java
@@ -0,0 +1,13 @@
+
+class Program2 {
+    public static void main(String[] args) {
+        CustomList cstObj = new CustomList();
+        cstObj.addNode(10);
+        cstObj.addNode(20);
+        cstObj.addNode(30);
+        cstObj.addNode(40);
+        cstObj.addNode(50);
+        // Printing the Custom List using toString method
+        System.out.println(cstObj);
+    }
+}
